8c9f3fef8500763776e3aa7a2486d073cb38971e
Alex <conquereralex@gmail.com>
2025-09-22 14:57:05 -0400

    add model train engine module

---
+++ modules/engine.py
@@ -0,0 +1,93 @@
+import torch
+from torch import optim,nn
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+from typing import Dict,List,Tuple
+
+def train_step(
+        model: nn.Module,
+        dataloader: DataLoader,
+        optimizer: optim.Optimizer,
+        loss_fn: nn.Module,
+        device: torch.device,
+) -> Tuple[float, float]:
+    model.train()
+    train_loss = 0
+    train_acc = 0
+    for batch,(X,y) in enumerate(DataLoader):
+        X, y = X.to(device), y.to(device)
+        y_pred = model(X)
+        loss = loss_fn(y_pred, y)
+        train_loss += loss.item()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+        train_acc += (y_pred_class == y).sum().item()/len(y_pred_class)
+
+    train_loss /= len(dataloader)
+    train_acc /= len(dataloader)
+    return train_loss, train_acc
+
+def test_step(
+        model: nn.Module,
+        dataloader: DataLoader,
+        loss_fn: nn.Module,
+        device: torch.device,
+)-> Tuple[float, float]:
+    model.eval()
+    test_loss = 0
+    test_acc = 0
+    with torch.inference_mode():
+        for batch,(X, y) in enumerate(DataLoader):
+            X,y = X.to(device), y.to(device)
+            test_pred_logits = model(X)
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
+
+            test_pred_labels = test_pred_logits.argmax(dim=1)
+            test_acc += (test_pred_labels == y ).sum().item()/len(test_pred_labels)
+
+    test_loss /= len(dataloader)
+    test_acc /= len(dataloader)
+    return test_loss, test_acc
+
+def train(
+        model: nn.Module,
+        train_dataloader: DataLoader,
+        test_dataloader: DataLoader,
+        optimizer: optim.Optimizer,
+        loss_fn: nn.Module,
+        epochs: int,
+        device: torch.device,
+)-> Dict[str,List]:
+    results = {
+        "train_loss": [],
+        "train_acc": [],
+        "test_loss": [],
+        "test_acc": [],
+    }
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(
+            model,
+            train_dataloader,
+            optimizer,
+            loss_fn,
+            device)
+        test_loss, test_acc = test_step(
+            model,
+            test_dataloader,
+            loss_fn,
+            device)
+        print(f"Epoch {epoch+1}|"
+              f" Train Loss: {train_loss:.3f}|"
+              f" Train Acc: {train_acc:.3f}|"
+              f" Test Loss: {test_loss:.3f}|"
+              f" Test Acc: {test_acc:.3f}"
+        )
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
+    return results
