bac31c5a510dd1ae410892c8eda9eccd1941742e
Alex <conquereralex@gmail.com>
2025-09-25 17:39:42 -0400

    created summary writer module

---
+++ modules/create_writer.py
@@ -0,0 +1,17 @@
+import torch.utils.tensorboard
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+import os
+def create_writer(
+        experiment_name: str,
+        model_name: str,
+        extra:str = None,
+) -> torch.utils.tensorboard.SummaryWriter:
+    timestamp = datetime.now().strftime("%Y-%m-%d, %H-%M-%S")
+
+    if extra:
+        log_dir = os.path.join('runs', timestamp,experiment_name,model_name,extra)
+    else:
+        log_dir = os.path.join('runs',timestamp,experiment_name,model_name)
+    print(f'[INFO] Created SummaryWriter. saving to {log_dir}...')
+    return SummaryWriter(log_dir=log_dir)
