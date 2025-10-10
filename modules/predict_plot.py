6493399e77f8832a3aa445aca44e09786a4e1f74
Alex <conquereralex@gmail.com>
2025-10-10 16:30:10 -0400

    update predict_plot script

--- modules/predict_plot.py
+++ modules/predict_plot.py
@@ -1,39 +1,43 @@
 import torch
 import torchvision
 from torchvision import transforms
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from torch import nn
 from typing import List, Tuple
 from PIL import Image
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 def pred_and_plot_image(
-        model:torch.nn.Module,
+        model: nn.Module,
         image_path:str,
-        class_names:List[str],
-        image_size:Tuple[int,int]=(224,224),
-        transform:torchvision.transforms = None,
-        device : torch.device = device,
-
+        class_names: List[str],
+        image_size: Tuple[int, int] = [224, 224],
+        transform: torchvision.transforms = None,
+        device: torch.device = device,
 ):
-    image = Image.open(image_path)
+    img = Image.open(image_path)
     if transform is not None:
         image_transform = transform
     else:
-        image_transform = transforms.Compose([
+        image_transform = torchvision.transforms.Compose([
             transforms.Resize(image_size),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
         ])
 
-        model.to(device)
+    model.to(device)
 
-        model.eval()
-        with torch.inference_mode():
-            transformed_image = image_transform(image).unsqueeze(0).to(device)
-            target_image_pred = model(transformed_image).to(device)
-        target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
-        target_image_pred_labels = torch.argmax(target_image_pred_probs, dim=1)
-        plt.figure()
-        plt.imshow(image)
-        plt.axis('off')
-        plt.title(f"Predictions: {class_names[target_image_pred_labels]}|Probabilities: {target_image_pred_probs.max():.3f}")
+    model.eval()
+    with torch.inference_mode():
+        transformed_image = image_transform(img).unsqueeze(0)
+
+        target_image_pred = model(transformed_image).to(device)
+
+    target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+    target_image_pred_labels = torch.argmax(target_image_pred_probs, dim=1)
+
+    plt.figure()
+    plt.imshow(img)
+    plt.title(f'Prediction: {class_names[target_image_pred_labels]}| Probability: {target_image_pred_probs.max():.3f}')
+    plt.axis('off')
