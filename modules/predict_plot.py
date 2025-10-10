f55f18c6f5ec8a5e4011116aaf8bf10afde7cdec
Alex <conquereralex@gmail.com>
2025-10-10 15:58:56 -0400

    update image size type hint

--- modules/predict_plot.py
+++ modules/predict_plot.py
@@ -9,7 +9,7 @@ def pred_and_plot_image(
         model:nn.Module,
         image_path:str,
         class_names:List[str],
-        image_size:Tuple[int]=[224,224],
+        image_size:Tuple[int,int]=(224,224),
         transform:transforms = None,
         device : torch.device = device,
 
